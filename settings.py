cf00e2be7de54e3b29f7483bba6990535ac05019
feride <ferideosm@gmail.com>
2022-11-22 18:45:39 +0300

    static add

--- settings.py
+++ settings.py
@@ -16,7 +16,8 @@ import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -137,7 +138,6 @@ STATIC_URL = "/static/"
 django_heroku.settings(locals())
 # Enable WhiteNoise's GZip compression of static assets.
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '/static'), )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
